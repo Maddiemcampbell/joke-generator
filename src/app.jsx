59b273983afa81dae50de648107372e6703fa5f9
Maddie <maddiemcampbell@gmail.com>
2019-10-09 15:12:02 -0700

    added some styling

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            jokeRandom:[],
+            jokeRandom: [],
             jokeProgramming: []
         }
         this.handleRandomJoke = this.handleRandomJoke.bind(this);
@@ -14,51 +14,47 @@ export default class App extends React.Component {
 
     handleProgrammingJoke() {
         axios
-          .get('https://sv443.net/jokeapi/category/Programming')
-          .then(response => response.data)
-          .then( jokeProgramming => {
-              console.log(jokeProgramming.joke)            
-              this.setState({ 
-                  jokeProgramming: jokeProgramming.joke,
-              });
-              console.log(this.state.jokeProgramming);
-          })
-        }
+            .get('https://sv443.net/jokeapi/category/Programming')
+            .then(response => response.data)
+            .then(jokeProgramming => {
+                this.setState({
+                    jokeProgramming: jokeProgramming.joke,
+                });
+            })
+    }
 
     handleRandomJoke() {
         axios
-          .get('http://api.icndb.com/jokes/random/')
-          .then(response => response.data)
-          .then( jokeRandom => {
-              console.log(jokeRandom.value.joke)            
-              this.setState({ 
-                  jokeRandom: jokeRandom.value.joke,
-              });
-              console.log(this.state.jokeRandom);
-          })
-        }
-        
-        
+            .get('http://api.icndb.com/jokes/random/')
+            .then(response => response.data)
+            .then(jokeRandom => {
+                this.setState({
+                    jokeRandom: jokeRandom.value.joke,
+                });
+            })
+    }
+
+
     render() {
         return (
-            <div className="App">
-            <div className="container">
-              <div>
-                <button className="btn" onClick={this.handleRandomJoke}>
-                  CLICK ME!
-                </button>
-                <h2 className=''>{this.state.jokeRandom}</h2>
-              </div>
-              <div className="container">
-              <div>
-                <button className="btn" onClick={this.handleProgrammingJoke}>
-                  CLICK ME!
-                </button>
-                <h2 className=''>{this.state.jokeProgramming}</h2>
-              </div>
+            <div className="app">
+                    <h1>A Joke For Everyone!</h1>
+                    <div className="container">
+                        <button className="butn" onClick={this.handleRandomJoke}>
+                            Random Joke!
+                        </button>
+                        <h2 className='joke'>{this.state.jokeRandom}</h2>
+                    </div>
+
+                    <div className="container">
+                        <div className="jokeWrapper">
+                            <button className="butn" onClick={this.handleProgrammingJoke}>
+                                Programmer Joke!
+                            </button>
+                            <h2 className='joke'>{this.state.jokeProgramming}</h2>
+                        </div>
+                    </div>
             </div>
-          </div>
-          </div>
         )
     }
 }
